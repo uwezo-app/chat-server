c68698a28d31b5a9d798c467d716d3e11173c8f1
lucienmakutano <lucien.programmer@gmail.com>
2021-07-16 11:32:38 +0300

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/uwezo-app/chat-server
+
+go 1.16
+
+require github.com/gorilla/websocket v1.4.2
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	ws "github.com/gorilla/websocket"
+)
+
+var upgrader = ws.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Fatal("Could upgrade the connection")
+		}
+
+		defer func(conn *ws.Conn) {
+			err := conn.Close()
+			if err != nil {
+				log.Println("Could not close the connection")
+			}
+		}(conn)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
